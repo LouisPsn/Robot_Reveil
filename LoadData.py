0a8e6e71faf7b999b3c17949f5ddd3155f9928f2
EVANS Jules <Jules.Evans@bordeaux-inp.fr>
2022-05-12 19:18:13 +0200

    added the files for loading a map

---
+++ LoadData.py
@@ -0,0 +1,43 @@
+import readline
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def load_data(file_name):
+    """
+    Loads the data from the file and returns a numpy array.
+    :param file_name: The name of the file to load.
+    :return: The data as a numpy array.
+    """
+    file = open(file_name, 'r')
+    nb_robots = int(file.readline())
+    Edges = np.zeros([nb_robots, nb_robots]);
+    RobotStart = []
+    RobotCoords = []
+    Obstacles = []
+    for line in file:
+        numb = ""
+        if line[0] == 'R':
+            RobotStart.append(int(line.split(':')[1].split(',')[0].split('(')[1]))
+            RobotStart.append(int(line.split(':')[1].split(',')[1].split(')')[0]))
+        elif line[0] == 'X':
+            Obstacles.append([[int(line.split(':')[1].split(';')[0].split(',')[0].split('(')[1]), int(line.split(':')[1].split(';')[0].split(',')[1].split(')')[0])],
+             [int(line.split(':')[1].split(';')[1].split(',')[0].split('(')[1]), int(line.split(':')[1].split(';')[1].split(',')[1].split(')')[0])]])
+        elif 48 <= ord(line[0]) <= 57:
+            numb += line[0]
+            k = 1
+            while(line[k] != ':'):
+                numb += line[k]
+                k += 1
+            RobotCoords.append([int(line.split(':')[1].split(',')[0].split('(')[1]), int(line.split(':')[1].split(',')[1].split(')')[0])])
+        elif line[0] == 'E':
+            O, T = line.split(':')[1].split(',')[0].split('(')[1], line.split(':')[1].split(',')[1].split(')')[0]
+            if O == 'R':
+                O = '0'
+            if T == 'R':
+                T = '0' 
+            Edges[int(O), int(T)] = 1
+        
+    file.close()
+    return RobotStart, RobotCoords, Obstacles, Edges
+
