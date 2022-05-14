5edc817072f27ddc873cee22558b02aaeea816b2
EVANS Jules <Jules.Evans@bordeaux-inp.fr>
2022-05-14 17:13:12 +0200

    added test for the crossing_segments func

--- obstacle.py
+++ obstacle.py
@@ -1,8 +1,12 @@
-from enum import Flag
-from tracemalloc import start
-from turtle import distance
+# from enum import Flag
+# from tracemalloc import start
+# from turtle import distance
 import numpy as np
 
+def check_point_between_coords(point, coord1, coord2):
+    if coord1 > coord2:
+        coord1, coord2 = coord2, coord1
+    return coord1 <= point <= coord2
 
 def crossing_segments(seg1, seg2):
     a1 = (seg1[1][1] - seg1[0][1])/(seg1[1][0] - seg1[0][0])
@@ -21,25 +25,16 @@ def crossing_segments(seg1, seg2):
 
     x = X[0]
     y = X[1]
-
-    return ((x > seg1[0][0]) and (x < seg1[1][0]) and (x > seg2[0][0]) and (x < seg2[1][0]))
+    #checking if the intersection is on the segment.
+    return check_point_between_coords(x, seg1[0][0], seg1[1][0]) and check_point_between_coords(y, seg1[0][1], seg1[1][1]) and check_point_between_coords(x, seg2[0][0], seg2[1][0]) and check_point_between_coords(y, seg2[0][1], seg2[1][1])
 
 
 def edge_through_obstacle(edge, obs):
     for i in range (len(obs)):
         return 0
 
-
-
 def calculate_distance_obstacle(grid, start, end):
     return 0
-
-
-def test_crossing_segments():
-    seg1 = [[0,0], [1,1]]
-    seg2 = [[0,1], [1,0]]
-    print(crossing_segments(seg1, seg2))
-
 
 if __name__ == '__main__':
     grid = np.array([[24, 42],
@@ -47,6 +42,4 @@ if __name__ == '__main__':
                     [1, 3, 6], 
                     [2, 25, 4], 
                     ['X', (24,8), (26,24)]], dtype=object)
-
     # print(calculate_distance_obstacle(grid, (10, 10), (3, 6)))
-    test_crossing_segments()
